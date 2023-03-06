a7fd093bc2ff6cdb7fa3461dba2e4184da6c3409
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-06 16:10:23 +0300

    Reaturant Info

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Searchbar } from "react-native-paper";
 import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 
+import { RestaurantInfo } from "../components/restaurant-info.component";
+
+
 export const RestaurantsScreen = () => (
   <SafeAreaView style={styles.container}>
     <View style={styles.search}>
       <Searchbar />
     </View>
     <View style={styles.list}>
-      <Text>list</Text>
+      <RestaurantInfo/>
     </View>
   </SafeAreaView>
 );
