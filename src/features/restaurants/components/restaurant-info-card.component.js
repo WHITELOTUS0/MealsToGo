4559d4111e6411b87667dffa2d180ca7beaaec2f
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-06 19:33:02 +0300

    rating

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -12,7 +15,7 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-const Address=styled(Text)`
+const Address = styled(Text)`
 font-family: ${(props) => props.theme.fonts.body};
 font-size:${(props) => props.theme.fontSizes.caption};
 `;
@@ -27,6 +30,13 @@ const Info = styled.View`
 padding: ${(props) => props.theme.space[3]}
 `;
 
+const Rating = styled.View`
+flex-direction: row;
+padding-top: ${(props) => props.theme.space[2]};
+padding-bottom: ${(props) => props.theme.space[2]}
+
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some restaurant",
@@ -39,12 +49,20 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-      <Title>{name}</Title>
-      <Address>{address}</Address>
+        <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
+        <Address>{address}</Address>
       </Info>
     </RestaurantCard>
   );
