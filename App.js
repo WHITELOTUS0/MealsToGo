a3979c9f15c58235eef265225ea0e02bef9844d1
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-07 22:37:04 +0300

    bottom tab

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import {
   useFonts as useOswald,
@@ -14,6 +16,8 @@ import {
 
 import { theme } from "./src/infrastructure/theme"
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen.js";
+
+const Tab=createBottomTabNavigator();
 
 export default function App() {
 
@@ -32,7 +36,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -15,8 +15,8 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantList=styled(FlatList).attrs({
-  contentContainerStyle:{ padding: 16 }
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: { padding: 16 }
 })``;
 
 export const RestaurantsScreen = () => (
@@ -27,9 +27,9 @@ export const RestaurantsScreen = () => (
     <RestaurantList
       data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }, { name: 5 }, { name: 6 }, { name: 7 }, { name: 8 }, { name: 9 }, { name: 10 }, { name: 11 }, { name: 12 }, { name: 13 }, { name: 14 }]}
       renderItem={() => <>
-      <Spacer position="bottom" size="large">
-      <RestaurantInfoCard />
-      </Spacer>
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
       </>}
       keyExtractor={(item) => item.name}
     />
