5e8f7ddb44b287fb9b905e71b6f59cc34d79759b
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-07 23:31:44 +0300

    Bottom Navigation and Tab Navigator

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import {
   useFonts as useOswald,
@@ -19,10 +20,10 @@ import { theme } from "./src/infrastructure/theme"
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen.js";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 
-const Tab=createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-const Settings=()=>(<SafeArea><Text>Settings</Text></SafeArea>);
-const Map=()=>(<SafeArea><Text>Map</Text></SafeArea>);
+const Settings = () => (<SafeArea><Text>Settings</Text></SafeArea>);
+const Map = () => (<SafeArea><Text>Map</Text></SafeArea>);
 
 
 export default function App() {
@@ -35,7 +36,7 @@ export default function App() {
     Lato_400Regular,
   });
 
-  if(!oswaldLoaded|| !latoLoaded){
+  if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
 
@@ -43,10 +44,31 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen}/>
-            <Tab.Screen name="Maps" component={Map}/>
-            <Tab.Screen name="Settings" component={Settings}/>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                if (route.name === 'Restaurants') {
+                  iconName = "md-restaurant";
+                } else if (route.name === 'Settings') {
+                  iconName = "md-settings";
+                } else if (route.name === 'Map') {
+                  iconName = "md-map";
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: 'tomato',
+              tabBarInactiveTintColor: 'gray',
+            })}
+
+          >
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
           </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
