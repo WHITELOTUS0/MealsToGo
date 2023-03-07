501ba83d1001996581669f73aaff763cf1b9ac3f
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-07 21:12:13 +0300

    FlatLists

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -10,7 +10,7 @@ import { Icon, RestaurantCard, RestaurantCardCover, Address, Rating, Section, Se
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Some restaurant",
+    name = "Some Restaurant",
     icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
