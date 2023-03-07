9b95ad630149746de0abb21445409110620081f3
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-07 23:08:00 +0300

    Bottom Tabs

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -16,8 +17,13 @@ import {
 
 import { theme } from "./src/infrastructure/theme"
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen.js";
+import { SafeArea } from "./src/components/utility/safe-area.component";
 
 const Tab=createBottomTabNavigator();
+
+const Settings=()=>(<SafeArea><Text>Settings</Text></SafeArea>);
+const Map=()=>(<SafeArea><Text>Map</Text></SafeArea>);
+
 
 export default function App() {
 
@@ -37,7 +43,11 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen}/>
+            <Tab.Screen name="Maps" component={Map}/>
+            <Tab.Screen name="Settings" component={Settings}/>
+          </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
