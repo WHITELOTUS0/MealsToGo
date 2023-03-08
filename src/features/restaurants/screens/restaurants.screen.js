9972ad092cabe3aa8c79dba6c97041b5aca86745
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-08 19:38:06 +0300

    Activity Indicator

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
+import { View } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { FlatList } from "react-native";
 import styled from "styled-components/native";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
@@ -19,17 +21,35 @@ const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: { padding: 16 }
 })``;
 
+const Loading=styled(ActivityIndicator)`
+margin-left:-25px;
+`;
+
+const LoadingContainer=styled.View`
+position:absolute;
+top:50%;
+left:50%;
+`;
+
 export const RestaurantsScreen = () => {
   const {isLoading,error,restaurants} = useContext(RestaurantsContext);
   return (
     <SafeArea>
+      {isLoading&& (
+        <LoadingContainer>
+        <Loading
+         size={50}
+         animating={true}
+         color="#FF6347"
+        />
+      </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
       <RestaurantList
         data={restaurants}
         renderItem={({item}) =>{ 
-          console.log(item);
         return(
           <Spacer position="bottom" size="large">
             <RestaurantInfoCard restaurant={item}/>
