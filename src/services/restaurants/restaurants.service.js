e2f577f92460e8e0ba8466dc1e155aa4a35bd214
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-03-08 17:58:28 +0300

    transform response

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -12,9 +12,16 @@ export const restaurantsRequest=(location="37.7749295,-122.4194155")=>{
     });
 };
 
-const restaurantsTransform=(result)=>{
-    return camelize(result);
-}
+const restaurantsTransform=({results=[]})=>{
+    const mappedResults=results.map((restaurant)=>{
+        return {
+            ...restaurant,
+            isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+            isClosedTemporarily: restaurant.business_status==="CLOSED_TEMPORARILY",
+        };
+    });
+    return camelize(mappedResults);
+};
 
 restaurantsRequest().then(restaurantsTransform)
 .then(transformedResponse=>{
