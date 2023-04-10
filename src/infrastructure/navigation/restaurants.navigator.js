639e98ff16942344acb2694fcf95f7ef2192d13f
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-10 16:55:48 +0300

    navigator restaurants

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,3 +1,11 @@
 import React from "react";
 
 import {createStackNavigator} from "@react-navigation/stack";
+
+const RestaurantStack=createStackNavigator();
+
+const RestaurantsNavigator=()=>{
+    return(
+        <RestaurantStack.Navigator/>
+    );
+}
