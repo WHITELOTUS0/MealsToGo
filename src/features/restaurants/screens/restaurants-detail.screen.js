ad351e9811ba61d00c434b7420adbc76389c2066
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-10 18:51:25 +0300

    RestaurantsDetailScreen

--- src/features/restaurants/screens/restaurants-detail.screen.js
+++ src/features/restaurants/screens/restaurants-detail.screen.js
@@ -3,3 +3,7 @@ import React from "react";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
+
+export const RestaurantsDetailScreen=({route})=>{
+    
+};
