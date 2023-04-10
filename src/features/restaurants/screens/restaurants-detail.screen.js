dddfd990f3ed4f39efe7c71b020c82edae4094b6
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-10 18:50:14 +0300

    imports

---
+++ src/features/restaurants/screens/restaurants-detail.screen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+import { SafeArea } from "../../../components/utility/safe-area.component";
