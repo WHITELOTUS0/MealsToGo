8278c076d8ac982d746f57af579334726bad40f9
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-11 20:36:38 +0300

    mapmarker

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -5,18 +5,27 @@ import styled from 'styled-components/native';
 import { SafeArea } from '../../../components/utility/safe-area.component';
 
 import { Search } from '../components/search.component';
+import { LocationContext } from '../../../services/location/location.context';
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context';
 
 const Map = styled(MapView)`
 height:100%;
 width:100%
 `;
 
-export const MapScreen = () => (
+export const MapScreen = () => {
+    const {location} = useContext(LocationContext);
+    const {restaurants = []} = useContext(RestaurantsContext);
+    return(
     <>
 
         <Search />
-        <Map />
+        <Map>
+            {restaurants.map((restaurant)=>{
+                return null;
+            })}
+        </Map>
 
     </>
-);
+)};
 
