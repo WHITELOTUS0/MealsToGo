d598b040303d55730b841abfee20f74f8e2a7dfe
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-11 19:55:29 +0300

    map screen styling

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,24 +1,20 @@
 import React, { useContext, useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import styled from 'styled-components/native';
 import { SafeArea } from '../../../components/utility/safe-area.component';
 
-export const MapScreen = () => {
-    return (
-        <SafeArea>
-        <View style={styles.container}>
-          <MapView style={styles.map} />
-        </View>
-        </SafeArea>
-      )
-}
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    map: {
-      width: '100%',
-      height: '100%',
-    },
-  });
+const Map = styled(MapView)`
+height:100%;
+width:100%
+`;
+
+export const MapScreen = () => (
+    <>
+    <SafeArea>
+    <Map/>
+    </SafeArea>
+    </>
+);
+
