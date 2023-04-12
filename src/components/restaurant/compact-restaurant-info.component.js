69091758d1f5492f1da0d440535b92c0bfc8021b
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-12 21:47:48 +0300

    compactimage

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components/native";
+
+
+const CompactImage=styled.Image`
+border-radius:10px;
+width:120px;
+height:100px
+`
+
+const Item= styled.View`
+padding:10px;
+max-width:120px
+align-items:center
+`
 
 export const CompactRestaurantInfo=()=>{
     return(
-        
+        <Item>
+            <CompactImage/>
+        </Item>
     )
 }
