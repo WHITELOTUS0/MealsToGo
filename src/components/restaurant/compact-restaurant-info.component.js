04b896b4e03666dde48d0f6bc981ff3b0b4cdb82
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-12 22:00:12 +0300

    restaurant map callout

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,24 +1,24 @@
 import React from "react";
 import styled from "styled-components/native";
-import {Text} from "../typography/text.component"
+import { Text } from "../typography/text.component"
 
 
-const CompactImage=styled.Image`
+const CompactImage = styled.Image`
 border-radius:10px;
 width:120px;
 height:100px
 `
 
-const Item= styled.View`
+const Item = styled.View`
 padding:10px;
 max-width:120px
 align-items:center
 `
 
-export const CompactRestaurantInfo=()=>{
-    return(
+export const CompactRestaurantInfo = ({ restaurant }) => {
+    return (
         <Item>
-            <CompactImage source={{uri:restaurantsRequest.photos[0]}} />
+            <CompactImage source={{ uri: restaurant.photos[0] }} />
             <Text center variant="caption" numberOfLines={3}>
                 {restaurant.name}
             </Text>
