52a4212c004749a1dd024289fd1832a412447f75
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-12 22:14:50 +0300

    check platform

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,9 +1,16 @@
 import React from "react";
 import styled from "styled-components/native";
+import { WebView } from 'react-native-webview';
+import { Platform } from "react-native";
 import { Text } from "../typography/text.component"
 
 
 const CompactImage = styled.Image`
+border-radius:10px;
+width:120px;
+height:100px
+`
+const CompactWebView = styled(WebView)`
 border-radius:10px;
 width:120px;
 height:100px
