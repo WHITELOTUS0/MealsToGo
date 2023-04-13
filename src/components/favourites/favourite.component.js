0bf8d848cc58a746277bb4c20699893eef444da9
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-13 12:12:10 +0300

    FavouritesButton style

--- src/components/favourites/favourite.component.js
+++ src/components/favourites/favourite.component.js
@@ -5,6 +5,16 @@ import { TouchableOpacity } from "react-native";
 
 import { FavouritesContext } from "../../services/favourites/favourites.context";
 
+const FavouriteButton = styled(TouchableOpacity)`
+background-color:transparent;
+border-color:#20232a;
+position:absolute;
+top:10px;
+right:10px;
+width:64px;
+z-index:9;
+`
+
 export const Favourite=()=>{
     const {favourites, addToFavourites, removeFromfavourites}= useContext(FavouritesContext);
     return (
