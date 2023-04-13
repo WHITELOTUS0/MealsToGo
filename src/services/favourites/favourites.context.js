7cc16056c9af975f7d74cabc3bb2b29493c9345e
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-13 11:29:42 +0300

    use state

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,7 +1,8 @@
-import React, {createContext} from "react";
+import React, {createContext, useState} from "react";
 
 export const FavouritesContext=createContext();
 
 export const FavouritesContextProvider=({children})=>{
+    const [favourites, setFavourites] = useState([]);
     return <FavouritesContext.Provider>{children}</FavouritesContext.Provider>
 }
