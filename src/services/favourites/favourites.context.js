0e1889bd87468a5676d976d372e19eda5b0c9836
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-13 11:26:56 +0300

    context provider

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,1 +1,7 @@
-import React from "react";
+import React, {createContext} from "react";
+
+export const FavouritesContext=createContext();
+
+export const FavouritesContextProvider=({children})=>{
+    return <FavouritesContext.Provider>{children}</FavouritesContext.Provider>
+}
