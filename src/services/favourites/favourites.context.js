f5541d3658c89d3423efb6c84d8838e27f337f0a
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-13 11:36:11 +0300

    favourites add and remove

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -4,5 +4,15 @@ export const FavouritesContext=createContext();
 
 export const FavouritesContextProvider=({children})=>{
     const [favourites, setFavourites] = useState([]);
+
+    const add = (restaurant)=>{
+        setFavourites([...favourites, restaurant]);
+    };
+
+    const remove=(restaurant)=>{
+        const newFavourites = favourites.filter((x)=>x.placeId!==restaurant.placeId);
+        setFavourites(newFavourites);
+    }
+
     return <FavouritesContext.Provider>{children}</FavouritesContext.Provider>
 }
