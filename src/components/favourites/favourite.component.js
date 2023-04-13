4bd197f5298f64f820c416b90d7b7c19f90f0cba
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-13 12:09:16 +0300

    imports

--- src/components/favourites/favourite.component.js
+++ src/components/favourites/favourite.component.js
@@ -1,4 +1,7 @@
 import React, {useContext} from "react";
+import styled from "styled-components";
+import {AntDesign} from "@expo/vector-icons"
+import { TouchableOpacity } from "react-native";
 
 import { FavouritesContext } from "../../services/favourites/favourites.context";
 
