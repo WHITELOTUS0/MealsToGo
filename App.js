77dae5e658dbbee808ac6a6ceead238108ebcd29
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-15 23:14:33 +0300

    firebase init

--- App.js
+++ App.js
@@ -26,6 +26,19 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
+const firebaseConfig = {
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+};
+
+const firebaseInit = initializeApp(firebaseConfig);
+
 const Tab = createBottomTabNavigator();
 
 const Settings = () => (<SafeArea><Text>Settings</Text></SafeArea>);
