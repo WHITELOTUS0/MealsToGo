ac6a9038332718bc47dece14afb040ec87cc909c
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-15 22:34:22 +0300

    refinements

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -4,37 +4,54 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
-  const [favourites, setFavourites] = useState([]);
+    const [favourites, setFavourites] = useState([]);
 
-  const saveFavourites = async (value) => {
+    const saveFavourites = async (value) => {
+        try {
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem('favourites', jsonValue)
+        } catch (e) {
+            console.log("error storing",e);
+        }
+    }
+
+
+    
+const loadFavourites = async () => {
     try {
-      const jsonValue = JSON.stringify(value)
-      await AsyncStorage.setItem('@storage_Key', jsonValue)
-    } catch (e) {
-      // saving error
+      const value = await AsyncStorage.getItem('favourites')
+      if(value !== null) {
+        // value previously stored
+        setFavourites(JSON.parse(value))
+      }
+    } catch(e) {
+      // error reading value
+      console.log("error loading", e)
     }
   }
+  
 
-  const add = (restaurant) => {
-    setFavourites([...favourites, restaurant]);
-  };
 
-  const remove = (restaurant) => {
-    const newFavourites = favourites.filter(
-      (x) => x.placeId !== restaurant.placeId
+    const add = (restaurant) => {
+        setFavourites([...favourites, restaurant]);
+    };
+
+    const remove = (restaurant) => {
+        const newFavourites = favourites.filter(
+            (x) => x.placeId !== restaurant.placeId
+        );
+
+        setFavourites(newFavourites);
+    };
+    return (
+        <FavouritesContext.Provider
+            value={{
+                favourites,
+                addToFavourites: add,
+                removeFromFavourites: remove,
+            }}
+        >
+            {children}
+        </FavouritesContext.Provider>
     );
-
-    setFavourites(newFavourites);
-  };
-  return (
-    <FavouritesContext.Provider
-      value={{
-        favourites,
-        addToFavourites: add,
-        removeFromFavourites: remove,
-      }}
-    >
-      {children}
-    </FavouritesContext.Provider>
-  );
 };
