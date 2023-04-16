dab9316d744847a17fe536b434ebfaf3b34f5eec
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-16 13:41:30 +0300

    account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { ImageBackground } from "react-native";
 
+import { AccountBackground } from "../components/account.styles";
 
-const AccountScreen = ()=>{
-    return(
-        <ImageBackground></ImageBackground>
-    )
-}
+export const AccountScreen = () => {
+  return <AccountBackground />;
+};
