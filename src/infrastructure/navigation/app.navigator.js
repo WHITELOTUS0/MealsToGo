6059c87d32a616fb9aeaf6dea3219b73ddee1ea9
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-16 13:16:23 +0300

    Account and Authentication Navigation

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { Text } from "react-native";
@@ -14,7 +13,6 @@ const Tab = createBottomTabNavigator();
 const Settings = () => (<SafeArea><Text>Settings</Text></SafeArea>);
 
 export const AppNavigator = () => (
-  <NavigationContainer>
     <Tab.Navigator
       screenOptions={({ route }) => ({
         headerShown: false,
@@ -41,6 +39,6 @@ export const AppNavigator = () => (
       <Tab.Screen name="Map" component={MapScreen} />
       <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
-  </NavigationContainer>
+  
 
 );
--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,8 +1,18 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
+import { View, Text } from "react-native";
 import { AppNavigator } from "./app.navigator";
+import { NavigationContainer } from "@react-navigation/native";
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
+import { AccountNavigator } from "./account.navigator";
 
 export const Navigation = () => {
-    const {isAuthenticated}= useContext(AuthenticationContext);
-    return <AppNavigator />;
+    const { isAuthenticated } = useContext(AuthenticationContext);
+    return (
+        <NavigationContainer>
+            {isAuthenticated ? <AppNavigator /> : (
+                <AccountNavigator />
+            )}
+        </NavigationContainer>
+    )
+
 }
