8d69b23111752055b87b4a227101541ab106e379
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-16 13:57:26 +0300

    lighten up background

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { AccountBackground } from "../components/account.styles";
+import { AccountBackground, AccountCover } from "../components/account.styles";
 
 export const AccountScreen = () => {
-  return <AccountBackground />;
+  return <AccountBackground >
+    <AccountCover/>
+  </AccountBackground>;
 };
