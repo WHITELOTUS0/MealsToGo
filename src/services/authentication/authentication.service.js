ea77ce48a1354b8c90564401636ce51880675725
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-20 19:06:29 +0300

    Update authentication.service.js

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,7 +1,7 @@
 //import * as firebase from "firebase";
 import firebase from 'firebase/compat/app';
-//import 'firebase/compat/auth';
+import 'firebase/compat/auth';
 
-export const loginRequest=((email, password)=>{
+export const loginRequest=(email, password)=>{
     firebase.auth().signInWithEmailAndPassword(email, password)
-})
+};
