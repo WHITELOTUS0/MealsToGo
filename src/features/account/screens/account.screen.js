b68e86237a92a3be70e3d558a3fc6ade0f0a9ea8
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-21 19:44:52 +0300

    Title account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
 
-import { AccountBackground, AccountContainer, AccountCover, AuthButton } from "../components/account.styles";
+import { AccountBackground, AccountContainer, AccountCover, AuthButton, Title } from "../components/account.styles";
 
 export const AccountScreen = ({navigation}) => {
   return <AccountBackground >
     <AccountCover/>
+    <Title>Meals To Go</Title>
     <AccountContainer>
         <AuthButton
         icon="lock-open-outline"
