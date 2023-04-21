befee97863bc94f894899c77fa29e2bb7d9c74be
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-21 20:24:11 +0300

    Update App.js

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+export const auth = getAuth(app);
 
 
 const Tab = createBottomTabNavigator();
