ef5dbf88ad49e7655603c5785636c9e4e91e86d9
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-21 19:23:18 +0300

    Update authentication.context.js

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,6 +1,6 @@
 import React, {useState, createContext} from "react";
 //import * as firebase from "firebase";
-import firebase from 'firebase/compat/app';
+//import firebase from 'firebase/compat/app';
 //import 'firebase/compat/auth';
 
 import { loginRequest } from "./authentication.service";
@@ -8,7 +8,7 @@ import { loginRequest } from "./authentication.service";
 export const AuthenticationContext=createContext();
 
 export const AuthenticationContextProvider=({children})=>{
-    const [isLoading, setIsLoading]=useState(true);
+    const [isLoading, setIsLoading]=useState(false);
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
 
