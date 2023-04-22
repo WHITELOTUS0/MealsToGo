704b1ab666c76d44b2b8283f3a4aeb621999dd29
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 18:26:12 +0300

    Update settings.screens.js

--- src/features/settings/screens/settings.screens.js
+++ src/features/settings/screens/settings.screens.js
@@ -1,6 +1,6 @@
 import React,{useContext} from "react";
 
-import { Text, Button } from "react-native";
+
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
@@ -9,7 +9,6 @@ export const SettingsScreen = () => {
     const { onLogout } = useContext(AuthenticationContext);
     return (
       <SafeArea>
-        <Text>Settings</Text>
-        <Button title="logout" onPress={() => onLogout()}/>
+
       </SafeArea>)
   };
