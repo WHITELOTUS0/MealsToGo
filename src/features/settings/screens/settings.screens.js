daec26e3e31fc28ef98673e9f633f15f18e5556d
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 19:13:05 +0300

    Update settings.screens.js

--- src/features/settings/screens/settings.screens.js
+++ src/features/settings/screens/settings.screens.js
@@ -5,7 +5,8 @@ import { List } from "react-native-paper";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-const SettingsItem = styled(List.item)`
+const SettingsItem = styled(List.Item)`
+padding: ${(props)=>props.theme.space[3]}
 `;
 
 export const SettingsScreen = ({ navigation }) => {
@@ -13,15 +14,13 @@ export const SettingsScreen = ({ navigation }) => {
   return (
     <SafeArea>
       <List.Section>
-        <List.Item
-          style={{ padding: 16 }}
+        <SettingsItem
           title="Favourites"
           description="View your favourites"
           left={(props) => <List.Icon {...props} color="black" icon="heart" />}
           onPress={() => navigation.navigate("Favourites")}
         />
-        <List.Item
-          style={{ padding: 16 }}
+        <SettingsItem
           title="Logout"
           left={(props) => <List.Icon {...props} color="black" icon="door" />}
           onPress={onLogout}
