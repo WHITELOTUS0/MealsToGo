fc7820f6e390ede7e136319b2d3b38fc8d0e468a
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 16:48:31 +0300

    onLogout

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -64,6 +64,7 @@ export const AuthenticationContextProvider=({children})=>{
             error,
             onLogin,
             onRegister,
+            onLogout,
         }}
         >
             {children}
