aff6afd488fd90d5c3a53eaa38b7ede93463e779
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 17:17:00 +0300

    Update app.navigator.js

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,4 +1,4 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { Text, Button } from "react-native";
@@ -13,40 +13,46 @@ const Tab = createBottomTabNavigator();
 
 const Settings = () => {
   const { onLogout } = useContext(AuthenticationContext);
-  return(
-  <SafeArea>
-    <Text>Settings</Text>
-    <Button title="logout" onPress={() => onLogout()}></Button>
-  </SafeArea>)
-  };
+  return (
+    <SafeArea>
+      <Text>Settings</Text>
+      <Button title="logout" onPress={() => onLogout()}></Button>
+    </SafeArea>)
+};
 
 export const AppNavigator = () => (
-  <Tab.Navigator
-    screenOptions={({ route }) => ({
-      headerShown: false,
-      tabBarIcon: ({ color, size }) => {
-        let iconName;
+  <FavouritesContextProvider>
+    <LocationContextProvider>
+      <RestaurantsContextProvider>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
 
-        if (route.name === 'Restaurants') {
-          iconName = "md-restaurant";
-        } else if (route.name === 'Settings') {
-          iconName = "md-settings";
-        } else if (route.name === 'Map') {
-          iconName = "md-map";
-        }
+              if (route.name === 'Restaurants') {
+                iconName = "md-restaurant";
+              } else if (route.name === 'Settings') {
+                iconName = "md-settings";
+              } else if (route.name === 'Map') {
+                iconName = "md-map";
+              }
 
-        // You can return any component that you like here!
-        return <Ionicons name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: 'tomato',
-      tabBarInactiveTintColor: 'gray',
-    })}
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+          })}
 
-  >
-    <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-    <Tab.Screen name="Map" component={MapScreen} />
-    <Tab.Screen name="Settings" component={Settings} />
-  </Tab.Navigator>
+        >
+          <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+          <Tab.Screen name="Map" component={MapScreen} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </RestaurantsContextProvider>
+    </LocationContextProvider>
+  </FavouritesContextProvider>
 
 
 );
