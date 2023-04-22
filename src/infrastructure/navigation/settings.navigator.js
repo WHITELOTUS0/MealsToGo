da37e7484680976f2242fdae7d311c8a64014c15
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 18:38:58 +0300

    Update settings.navigator.js

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -1,3 +1,7 @@
 import React, {useEffect} from "react";
 
 import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
+
+export const SettingsNavigatsor=({route, navigation})=>{
+    return()
+}
