dc75d53463c60d8c56679d71bec36b03b2131517
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 17:32:08 +0300

    Update favourites.context.js

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -48,13 +48,13 @@ export const FavouritesContextProvider = ({ children }) => {
     };
 
     useEffect(() => {
-        if (user) {
+        if (user && user.uid) {
             loadFavourites(user.uid);
         }
     }, [user])
 
     useEffect(() => {
-        if(user){
+        if(user && user.uid && favourites.length){
         saveFavourites(favourites, user.uid);
         }
     }, [favourites, user])
