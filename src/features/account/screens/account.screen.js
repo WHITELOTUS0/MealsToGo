89d4d9e0f3906cfa4474be5f6b672adfb8752b1f
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 17:52:20 +0300

    Update account.screen.js

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import LottieView from 'lottie-react-native';
+
 import { Spacer } from "../../../components/spacer/spacer.component";
 
 import { AccountBackground, AccountContainer, AccountCover, AuthButton, Title } from "../components/account.styles";
