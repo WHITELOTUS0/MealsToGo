8e79ac907d65d2b923f4b4c97211107a0294838a
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 18:18:36 +0300

    imports

--- src/features/settings/screens/settings.screens.js
+++ src/features/settings/screens/settings.screens.js
@@ -1,5 +1,8 @@
 import { Text, Button } from "react-native";
 
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
 export const SettingScreen = () => {
     const { onLogout } = useContext(AuthenticationContext);
     return (
