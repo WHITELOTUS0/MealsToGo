5fe801f932e7d8e79903ad3e38e5b4af92691f7e
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 18:39:51 +0300

    createStackNavigator

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -2,6 +2,10 @@ import React, {useEffect} from "react";
 
 import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
 
+const SettingsStack = createStackNavigator();
+
 export const SettingsNavigatsor=({route, navigation})=>{
-    return()
+    return(
+
+    )
 }
