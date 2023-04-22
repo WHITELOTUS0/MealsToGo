44a265493513039103fc469495e75dd16b5f2918
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 16:50:14 +0300

    Update app.navigator.js

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -10,35 +10,38 @@ import { MapScreen } from "../../features/map/screens/map.screen";
 
 const Tab = createBottomTabNavigator();
 
-const Settings = () => (<SafeArea><Text>Settings</Text></SafeArea>);
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>);
 
 export const AppNavigator = () => (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarIcon: ({ color, size }) => {
+        let iconName;
 
-          if (route.name === 'Restaurants') {
-            iconName = "md-restaurant";
-          } else if (route.name === 'Settings') {
-            iconName = "md-settings";
-          } else if (route.name === 'Map') {
-            iconName = "md-map";
-          }
+        if (route.name === 'Restaurants') {
+          iconName = "md-restaurant";
+        } else if (route.name === 'Settings') {
+          iconName = "md-settings";
+        } else if (route.name === 'Map') {
+          iconName = "md-map";
+        }
 
-          // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}
 
-    >
-      <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
-  
+  >
+    <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+    <Tab.Screen name="Map" component={MapScreen} />
+    <Tab.Screen name="Settings" component={Settings} />
+  </Tab.Navigator>
+
 
 );
