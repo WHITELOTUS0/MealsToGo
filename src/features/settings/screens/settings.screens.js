f00129600da2acdc74603fa5e88a9ff90a8d1c92
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 18:35:10 +0300

    Update settings.screens.js

--- src/features/settings/screens/settings.screens.js
+++ src/features/settings/screens/settings.screens.js
@@ -5,12 +5,23 @@ import { List } from "react-native-paper";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-export const SettingsScreen = () => {
+export const SettingsScreen = ({navigation}) => {
     const { onLogout } = useContext(AuthenticationContext);
     return (
       <SafeArea>
         <List.Section>
-            
+        <List.Item
+            style={{padding:16}}
+            title="Favourites"
+            left={(props)=><List.Icon {...props} color="black" icon=""/>}
+            onPress={()=>navigation.navigate("Favourites")}
+            />
+            <List.Item
+            style={{padding:16}}
+            title="Logout"
+            left={(props)=><List.Icon {...props} color="black" icon=""/>}
+            onPress={onLogout}
+            />
         </List.Section>
       </SafeArea>)
   };
