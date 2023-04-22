a9d5d32cde46bb1854d8dac495486c523c116244
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 18:42:05 +0300

    Update settings.navigator.js

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -1,11 +1,17 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
 
 const SettingsStack = createStackNavigator();
 
-export const SettingsNavigatsor=({route, navigation})=>{
-    return(
-
+export const SettingsNavigatsor = ({ route, navigation }) => {
+    return (
+        <SettingsStack.Navigator
+            headerMode="screen"
+            screenOptions={{
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+            }}
+        >
+        </SettingsStack.Navigator>
     )
 }
