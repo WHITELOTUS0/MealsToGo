16a39c7667d284a2fe9166061220aaa6fbb819f2
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-22 15:23:28 +0300

    activity indicator

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,4 +1,6 @@
 import React,{useState, useContext} from "react";
+import { ActivityIndicator } from "react-native-paper";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 import { AccountBackground, ErrorContainer } from "../components/account.styles";
 
@@ -16,7 +18,7 @@ import { AuthenticationContext } from "../../../services/authentication/authenti
 export const LoginScreen = ({navigation}) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const { onLogin, error } = useContext(AuthenticationContext);
+    const { onLogin, error,isLoading } = useContext(AuthenticationContext);
     
   return (
     <AccountBackground>
@@ -47,13 +49,17 @@ export const LoginScreen = ({navigation}) => {
           </ErrorContainer>
         )}
         <Spacer size="large">
-          <AuthButton
+          {!isLoading?<AuthButton
             icon="lock-open-outline"
             mode="contained"
             onPress={() => onLogin(email, password)}
           >
             LOGIN
-          </AuthButton>
+          </AuthButton>:
+          (
+            <ActivityIndicator animating={true} color={Colors.blue300} />
+          )
+          }
         </Spacer>
       </AccountContainer>
       <Spacer size="large">
