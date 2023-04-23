e1b46307484cc6650292dde1b7a1fcced7481d18
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-23 17:46:40 +0300

    Update favourites.screen.js

--- src/features/settings/screens/favourites.screen.js
+++ src/features/settings/screens/favourites.screen.js
@@ -6,6 +6,8 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Text } from "../../../components/typography/text.component";
 
 const FavouritesArea=styled(SafeArea)`
+align-items:center;
+justify-content:center;
 `
 
 export const FavouritesScreen=()=>{
