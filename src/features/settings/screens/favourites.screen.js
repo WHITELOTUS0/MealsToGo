da33f506d48a3e2436936a86d1223b7ab40f9e1b
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-23 17:42:26 +0300

    Update favourites.screen.js

--- src/features/settings/screens/favourites.screen.js
+++ src/features/settings/screens/favourites.screen.js
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 
 import { FavouritesContext } from "../../../services/favourites/favourites.context";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Text } from "../../../components/typography/text.component";
 
 export const FavouritesScreen=()=>{
     const {favourites} = useContext(FavouritesContext);
     return favourites.length?
     ():
     (
-        <SafeArea>
+        <SafeArea></SafeArea>
     );
 };
