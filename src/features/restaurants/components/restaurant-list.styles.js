9f1a08cdd7368b5cba49875e8f52ff24ea09bb0d
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-23 17:58:46 +0300

    Update restaurant-list.styles.js

--- src/features/restaurants/components/restaurant-list.styles.js
+++ src/features/restaurants/components/restaurant-list.styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components/native";
 import { FlatList } from "react-native";
 
-const RestaurantList = styled(FlatList).attrs({
+export const RestaurantList = styled(FlatList).attrs({
     contentContainerStyle: { padding: 16 }
   })``;
