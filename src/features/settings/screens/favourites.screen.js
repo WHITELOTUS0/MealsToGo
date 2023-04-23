4cb3f6ff959a96a6034074ae8b0870e80e6d27b9
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-23 18:02:55 +0300

    Update favourites.screen.js

--- src/features/settings/screens/favourites.screen.js
+++ src/features/settings/screens/favourites.screen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
 
 import { FavouritesContext } from "../../../services/favourites/favourites.context";
 import { SafeArea } from "../../../components/utility/safe-area.component";
