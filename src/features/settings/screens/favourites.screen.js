062a2aa447907cf8c230ee8f1bf6b141d6746b37
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-23 17:37:21 +0300

    Update favourites.screen.js

--- src/features/settings/screens/favourites.screen.js
+++ src/features/settings/screens/favourites.screen.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { FavouritesContext } from "../../../services/favourites/favourites.context";
 
 export const FavouritesScreen=()=>{
-    
+    const {favourites} = useContext(FavouritesContext);
 };
