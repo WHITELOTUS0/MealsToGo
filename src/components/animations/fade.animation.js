db8c66808bb74f4f408b069ee3866cba83993c44
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-23 18:35:34 +0300

    Update fade.animation.js

--- src/components/animations/fade.animation.js
+++ src/components/animations/fade.animation.js
@@ -3,6 +3,15 @@ import { Animated } from "react-native";
 
 export const FadeInView=({duration=1500, ...props})=>{
     return(
-        
+        <Animated.View
+        style={[
+          styles.fadingContainer,
+          {
+            // Bind opacity to animated value
+            opacity: fadeAnim,
+          },
+        ]}>
+        <Text style={styles.fadingText}>Fading View!</Text>
+      </Animated.View>
     )
 };
