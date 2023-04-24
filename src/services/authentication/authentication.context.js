8123f03510ba365898220ec8a55a5505c06f33ab
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-24 22:42:36 +0300

    Update authentication.context.js

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,73 +1,73 @@
-import React, {useState, createContext} from "react";
+import React, { useState, createContext } from "react";
 //import * as firebase from "firebase";
 //import firebase from 'firebase/compat/app';
 //import 'firebase/compat/auth';
-import { createUserWithEmailAndPassword,onAuthStateChanged,signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../../App";
 
 import { loginRequest } from "./authentication.service";
 
-export const AuthenticationContext=createContext();
+export const AuthenticationContext = createContext();
 
-export const AuthenticationContextProvider=({children})=>{
-    const [isLoading, setIsLoading]=useState(false);
+export const AuthenticationContextProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(false);
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
 
     onAuthStateChanged(auth, (usr) => {
         if (usr) {
-         setUser(usr);
-         setIsLoading(false);
+            setUser(usr);
+            setIsLoading(false);
         } else {
-          setIsLoading(false);
+            setIsLoading(false);
         }
-      });
+    });
 
-    const onLogin=(email, password)=>{
+    const onLogin = (email, password) => {
         setIsLoading(true);
-        loginRequest(email, password).then((u)=>{
+        loginRequest(email, password).then((u) => {
             setUser(u);
             setIsLoading(false);
-        }).catch((e)=>{
+        }).catch((e) => {
             setIsLoading(false);
             setError(e.toString());
         })
     }
 
-    const onRegister=(email, password, repeatedPassword)=>{
+    const onRegister = (email, password, repeatedPassword) => {
         setIsLoading(true);
-        if(password!==repeatedPassword){
+        if (password !== repeatedPassword) {
             setError("Passwords do not match");
             return;
         }
-        createUserWithEmailAndPassword(auth,email,password)
-        .then((u)=>{
-            setUser(u);
-            setIsLoading(false);
-        }).catch((e)=>{
-            setIsLoading(false);
-            setError(e.toString());
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((u) => {
+                setUser(u);
+                setIsLoading(false);
+            }).catch((e) => {
+                setIsLoading(false);
+                setError(e.toString());
+            });
+    }
+
+    const onLogout = () => {
+        signOut(auth).then(() => {
+            setUser(null);
+            setError(null);
         });
     }
 
-    const onLogout=()=>{
-        signOut(auth) .then(() => {
-            setUser(null);
-            setError(null);
-          });
-    }
-
-    return(
+    return (
         <AuthenticationContext.Provider
-        value={{
-            user,
-            isAuthenticated:!!user,
-            isLoading,
-            error,
-            onLogin,
-            onRegister,
-            onLogout,
-        }}
+            value={{
+                user,
+                isAuthenticated: !!user,
+                isLoading,
+                error,
+                onLogin,
+                onRegister,
+                onLogout,
+            }}
         >
             {children}
         </AuthenticationContext.Provider>
