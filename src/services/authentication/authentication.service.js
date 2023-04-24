49648065a558faa380af37e38af52df00ac33460
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-24 22:41:47 +0300

    Update authentication.service.js

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -3,5 +3,5 @@ import firebase from 'firebase/compat/app';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../../App';
 
-export const loginRequest = async (email, password) => 
+export const loginRequest = async (email, password) =>
   signInWithEmailAndPassword(auth, email, password);
