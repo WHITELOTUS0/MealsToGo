79032611618f36d79f6840eb0f9d3dafa670b111
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-25 16:32:15 +0300

    Update camera.screen.js

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Camera } from "expo-camera";
+import styled from "styled-components/native";
 
 export const CameraScreen= ()=>{
     return <Camera style={{width:"100%", height:"100%"}}></Camera>
