5f14dc8b6d92f5a6763212177475e543ccbcd1d4
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-25 16:37:26 +0300

    Update camera.screen.js

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Camera, FlashMode } from "expo-camera";
 import styled from "styled-components/native";
 
@@ -8,5 +8,6 @@ width:100%;
 height:100%
 `
 export const CameraScreen= ()=>{
+    const cameraRef = useRef();
     return <ProfileCamera type={Camera.Constants.Type.front} flashMode={FlashMode.on}></ProfileCamera>
 };
