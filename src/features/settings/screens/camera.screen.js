db5efe6c2b8c02a20172d6a368a3be0ecdf25818
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-25 16:50:05 +0300

    Update camera.screen.js

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState,useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Camera, FlashMode } from "expo-camera";
 import styled from "styled-components/native";
 
@@ -8,18 +8,19 @@ width:100%;
 height:100%
 `
 export const CameraScreen = () => {
-    const [hasPersmission, setHasPermission]=useState(null);
+    const [hasPersmission, setHasPermission] = useState(null);
     const cameraRef = useRef();
 
-    useEffect(()=>{
-        (async ()=>{
-            
-        })
-    })
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, [])
 
     return (<ProfileCamera
         ref={(camera) => (cameraRef.current = camera)}
         type={Camera.Constants.Type.front}
         flashMode={FlashMode.on}>
-        </ProfileCamera>)
+    </ProfileCamera>)
 };
