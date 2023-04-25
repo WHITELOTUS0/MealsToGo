2f8b5e72ff45c06347f2f5e51342c019965226c7
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-04-25 16:01:00 +0300

    Update settings.screens.js

--- src/features/settings/screens/settings.screens.js
+++ src/features/settings/screens/settings.screens.js
@@ -9,10 +9,10 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const SettingsItem = styled(List.Item)`
-padding: ${(props)=>props.theme.space[3]}
+padding: ${(props) => props.theme.space[3]}
 `;
 
-const AvatarContainer=styled.View`
+const AvatarContainer = styled.View`
 align-items:center;
 `;
 
@@ -20,13 +20,15 @@ export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
   return (
     <SafeArea>
-        <AvatarContainer>
-        <Avatar.Icon size={180} icon="human"style={{backgroundColor:"#2182BD" }} />
+      <AvatarContainer>
+        <TouchableOpacity>
+          <Avatar.Icon size={180} icon="human" style={{ backgroundColor: "#2182BD" }} />
+        </TouchableOpacity>
         <Spacer position="top" size="large">
-            <Text variant="label">{user.email}</Text>
+          <Text variant="label">{user.email}</Text>
         </Spacer>
-        </AvatarContainer>
-        
+      </AvatarContainer>
+
       <List.Section>
         <SettingsItem
           title="Favourites"
